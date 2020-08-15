f21a7dd5b728a428192d4c9aec673558720d8bde
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-08-15 17:51:34 -0300

    Syntactical.Rules: fixing compiling problems and using some callbacks

--- src/syntactical/syntactical-rules.adb
+++ src/syntactical/syntactical-rules.adb
@@ -1,116 +1,260 @@
--- syntactical-rules.adb --- 
+--  syntactical-rules.adb ---
 
--- Copyright 2020 cnngimenez
+--  Copyright 2020 cnngimenez
 --
--- Author: cnngimenez
+--  Author: cnngimenez
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------
 
 package body Syntactical.Rules is
-    function Accept_Token (Token_Name : Token_Type; Value : Wide_Wide_String)
+
+    function Accept_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type)
                           return Boolean is
+        Token : Token_Type;
     begin
-        if Lexer.Accept_Token (Token_Name, Value) then
-            Lexer.Consume;
+        Token := Lexer.Peek_Token;
+
+        if Token.Get_Class = Token_Class then
+            Token := Lexer.Take_Token;
             return True;
         else
             return False;
         end if;
     end Accept_Token;
-    
-    function Expect_Token (Token_Name : Token_Type) return Boolean is
+
+    function Accept_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type;
+                           Value : Wide_Wide_String)
+                          return Boolean is
+        Token : Token_Type;
     begin
-        if Lexer.Accept_Token (Token_Name) then
-            Lexer.Consume;
+        Token := Lexer.Peek_Token;
+
+        if Token.Get_Class = Token_Class and then
+          Token.Get_Value = To_Universal_String (Value)
+        then
+            Token := Lexer.Take_Token;
             return True;
         else
-            --  Raise exception here
             return False;
         end if;
-    end Expect_Token;
+    end Accept_Token;
 
+    --  [5] base ::= '@base' IRIREF '.'
     function Base (Lexer : in out Lexer_Type) return Boolean is
+        Ret : Boolean;
+        Token : Token_Type;
     begin
-        return Accept_Token (Language_Tag, "@base") 
-          and then Expect_Token (Iri_Ref)
-          and then Expect_Token (Dot);
+        Ret := Accept_Token (Lexer, Language_Tag, "@base")
+          and then Expect_Token (Lexer, IRI_Reference, Token)
+          and then Expect_Token (Lexer, Reserved_Word, ".");
+
+        if Ret then
+            Base_Directive_Callback (Token.Get_Value);
+        end if;
+
+        return Ret;
     end Base;
-    
-    function Blank_Node (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Blank_Node_Property_List (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Boolean_Literal (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Collection (Lexer : in out Lexer_Type) return Boolean;
-    
+
+    function Blank_Node (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Blank_Node;
+
+    function Blank_Node_Property_List (Lexer : in out Lexer_Type)
+                                      return Boolean is
+    begin
+        return False;
+    end Blank_Node_Property_List;
+
+    function Boolean_Literal (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Boolean_Literal;
+
+    function Collection (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Collection;
+    --  [3] directive ::= prefixID | base | sparqlPrefix | sparqlBase
     function Directive (Lexer : in out Lexer_Type) return Boolean is
     begin
-        return Prefix_ID (Lexer) 
+        return Prefix_ID (Lexer)
           or else Base (Lexer)
           or else Sparql_Prefix (Lexer)
           or else Sparql_Base (Lexer);
     end Directive;
-    
-    function IRI (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Literal (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Numeric_Literal (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Object (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Object_List (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Predicate (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Predicate_Object_List (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Prefix_ID (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Prefixed_Name (Lexer : in out Lexer_Type) return Boolean;
-    
-    function RDF_Literal (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Sparql_Base (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Sparql_Prefix (Lexer : in out Lexer_Type) return Boolean;
 
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type;
+                           Token : in out Token_Type)
+                          return Boolean is
+    begin
+        Token := Lexer.Take_Token;
+
+        if Token.Get_Class = Token_Class then
+            return True;
+        else
+            raise Expected_Token_Exception with
+              "Expected token of class '" & Token_Class'Image &
+              "' and got '" & Token.Get_Class'Image & "'.";
+            --  return False;
+        end if;
+    end Expect_Token;
+
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type)
+                          return Boolean is
+        Token : Token_Type;
+    begin
+        return Expect_Token (Lexer, Token_Class, Token);
+    end Expect_Token;
+
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type;
+                           Value : Wide_Wide_String)
+                          return Boolean is
+        Token : Token_Type;
+        Ret : Boolean;
+    begin
+        Ret := Expect_Token (Lexer, Token_Class, Token);
+
+        if Ret and then Token.Get_Value = To_Universal_String (Value)
+        then
+            return True;
+        else
+            raise Expected_Token_Exception with
+              "Unexpected token value.";
+            --  return False;
+        end if;
+    end Expect_Token;
+
+    function IRI (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end IRI;
+
+    function Literal (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Literal;
+
+    function Numeric_Literal (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Numeric_Literal;
+
+    function Object (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Object;
+
+    function Object_List (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Object_List;
+
+    function Predicate (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Predicate;
+
+    function Predicate_Object_List (Lexer : in out Lexer_Type)
+                                   return Boolean is
+    begin
+        return False;
+    end Predicate_Object_List;
+
+    --  [4] prefixID ::= '@prefix' PNAME_NS IRIREF '.'
+    function Prefix_ID (Lexer : in out Lexer_Type) return Boolean is
+        Token_Prefix, Token_IRI : Token_Type;
+        Prefix : Prefix_Type;
+        Ret : Boolean;
+    begin
+        Ret :=  Accept_Token (Lexer, Language_Tag, "@prefix")
+          and then Expect_Token (Lexer, Prefix_Namespace, Token_Prefix)
+          and then Expect_Token (Lexer, IRI_Reference, Token_IRI)
+          and then Expect_Token (Lexer, Reserved_Word, ".");
+
+        if Ret then
+            Prefix.Initialize (Token_Prefix.Get_Value, Token_IRI.Get_Value);
+            Prefix_Directive_Callback (Prefix);
+        end if;
+
+        return Ret;
+    end Prefix_ID;
+
+    function Prefixed_Name (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Prefixed_Name;
+
+    function RDF_Literal (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end RDF_Literal;
+
+    function Sparql_Base (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Sparql_Base;
+
+    function Sparql_Prefix (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Sparql_Prefix;
+
+    --  [2] statement ::= directive | triples '.'
     function Statement (Lexer : in out Lexer_Type) return Boolean is
     begin
-        if not Directive (Lexer) then
-            if not Triples (Lexer) then
-                return False;
-            end if;
-        end if;
-        
-        Accept_Token (Dot);
+        return
+          (Directive (Lexer) or else Triples (Lexer))
+          and then Accept_Token (Lexer, Reserved_Word, ".");
     end Statement;
-    
-    function String (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Subject (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Triples (Lexer : in out Lexer_Type) return Boolean;
-    
-    function Turtle_Doc (Lexer : in out Lexer_Type) return Booleanis
+
+    function String (Lexer : in out Lexer_Type) return Boolean is
     begin
-        
+        return False;
+    end String;
+
+    function Subject (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Subject;
+
+    --  [6] triples ::= subject predicateObjectList
+    --    | blankNodePropertyList predicateObjectList?
+    function Triples (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        if Subject (Lexer) then
+            return Predicate_Object_List (Lexer);
+        else
+            return Blank_Node_Property_List (Lexer)
+              and then (Predicate_Object_List (Lexer) or else True);
+        end if;
+    end Triples;
+
+    function Turtle_Doc (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
     end Turtle_Doc;
-    
-    function Verb (Lexer : in out Lexer_Type) return Boolean;
-    
+
+    function Verb (Lexer : in out Lexer_Type) return Boolean is
+    begin
+        return False;
+    end Verb;
 end Syntactical.Rules;
--- src/syntactical/syntactical-rules.ads
+++ src/syntactical/syntactical-rules.ads
@@ -1,38 +1,49 @@
--- syntactical-rules.ads --- 
+--  syntactical-rules.ads ---
 
--- Copyright 2020 cnngimenez
+--  Copyright 2020 cnngimenez
 --
--- Author: cnngimenez
+--  Author: cnngimenez
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------
+with League.Strings;
+use League.Strings;
 
+with Lexical.Token;
+use Lexical.Token;
 with Lexical.Turtle_Lexer;
 use Lexical.Turtle_Lexer;
+with Elements.Triples;
+use Elements.Triples;
+with Elements.Prefixes;
+use Elements.Prefixes;
 
 generic
     --  This subprogram will be called every time a triple is parsed.
     with procedure Triple_Readed_Callback (Triple : Triple_Type);
-    --  This subprogram will be called every time a @prefix directive is parsed.
+    --
+    --  This subprogram will be called every time a @prefix directive is
+    --  parsed.
+    --
     with procedure Prefix_Directive_Callback (Prefix : Prefix_Type);
     --  This subprogram will be called every time a @base directive is parsed.
-    with procedure Base_Directive_Callback (Base : Base_Type);
+    with procedure Base_Directive_Callback (Base_IRI : Universal_String);
 package Syntactical.Rules is
     --  This is the main entry point for the syntactical analyser.
-    function Turtle_Doc (Lexer : in out Lexer_Type);
-    
+    function Turtle_Doc (Lexer : in out Lexer_Type) return Boolean;
+
     function Statement (Lexer : in out Lexer_Type) return Boolean;
     function Directive (Lexer : in out Lexer_Type) return Boolean;
     function Prefix_ID (Lexer : in out Lexer_Type) return Boolean;
@@ -47,7 +58,8 @@ package Syntactical.Rules is
     function Predicate (Lexer : in out Lexer_Type) return Boolean;
     function Object (Lexer : in out Lexer_Type) return Boolean;
     function Literal (Lexer : in out Lexer_Type) return Boolean;
-    function Blank_Node_Property_List (Lexer : in out Lexer_Type) return Boolean;
+    function Blank_Node_Property_List (Lexer : in out Lexer_Type)
+                                      return Boolean;
     function Collection (Lexer : in out Lexer_Type) return Boolean;
     function Numeric_Literal (Lexer : in out Lexer_Type) return Boolean;
     function RDF_Literal (Lexer : in out Lexer_Type) return Boolean;
@@ -56,15 +68,40 @@ package Syntactical.Rules is
     function IRI (Lexer : in out Lexer_Type) return Boolean;
     function Prefixed_Name (Lexer : in out Lexer_Type) return Boolean;
     function Blank_Node (Lexer : in out Lexer_Type) return Boolean;
-    
+
+    Expected_Token_Exception : exception;
+
 private
+
+    --
     --  Test if the next token is the given one. If it is, consume it.
     --  If it is not the one, do nothing.
+    --
     function Accept_Token (Lexer : in out Lexer_Type;
-                           Token_Name : Token_Type;
+                           Token_Class : Token_Class_Type;
                            Value : Wide_Wide_String)
                           return Boolean;
-    
+    --
+    --  Test if the next token is the given one. If it is, consume it.
+    --  If it is not the one, do nothing.
+    --
+    function Accept_Token (Lexer : in out Lexer_Type;
+                          Token_Class : Token_Class_Type)
+                         return Boolean;
+
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type;
+                           Token : in out Token_Type)
+                          return Boolean;
+
     --  The next token must be the given one, if not raise an exception.
-    function Expect_Token (Token_Name : Token_Type) return Boolean;
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type)
+                          return Boolean;
+
+        --  The next token must be the given one, if not raise an exception.
+    function Expect_Token (Lexer : in out Lexer_Type;
+                           Token_Class : Token_Class_Type;
+                           Value : Wide_Wide_String)
+                          return Boolean;
 end Syntactical.Rules;
