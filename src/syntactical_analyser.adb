d02f78392817664e93a989037abfe0101f1850cc
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-08-15 17:51:58 -0300

    Fixing main program style.

--- src/syntactical_analyser.adb
+++ src/syntactical_analyser.adb
@@ -1,21 +1,21 @@
--- syntactical_analyser.adb --- 
+--  syntactical_analyser.adb ---
 
--- Copyright 2020 cnngimenez
+--  Copyright 2020 cnngimenez
 --
--- Author: cnngimenez
+--  Author: cnngimenez
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------
 
@@ -26,59 +26,61 @@ use Ada.Wide_Wide_Text_IO;
 with Ada.Strings.Wide_Wide_Unbounded;
 use Ada.Strings.Wide_Wide_Unbounded;
 
+with Syntactical.Rules;
+with Elements.Triples;
+use Elements.Triples;
+with Elements.Prefixes;
+use Elements.Prefixes;
 with Source;
 use Source;
-with Lexical.Token;
-use Lexical.Token;
 with Lexical.Turtle_Lexer;
 use Lexical.Turtle_Lexer;
 with League.Strings;
 use League.Strings;
 
-procedure Lexical_Analizer is
-    
+procedure Syntactical_Analyser is
+
     procedure Print_Triple (Triple : Triple_Type);
     procedure Print_Prefix (Prefix : Prefix_Type);
-    procedure Print_Base (Base : Base_Type);
+    procedure Print_Base (Base : Universal_String);
     function Read_File (Path : String) return Wide_Wide_String;
-    
-    package Analyser is new Syntactical.Rules 
+
+    package Analyser is new Syntactical.Rules
       (
-       Triple_Readed_Callback => Print_Triple;
-       Prefix_Directive_Callback => Print_Prefix;
-       Base_Directive_Callback => Print_Base;
+       Triple_Readed_Callback => Print_Triple,
+       Prefix_Directive_Callback => Print_Prefix,
+       Base_Directive_Callback => Print_Base
       );
-    use Analyzer;
-    
+
+    procedure Print_Base (Base : Universal_String) is
+    begin
+        Put_Line ("Base IRI :");
+        Put_Line (To_Wide_Wide_String (Base));
+    end Print_Base;
+
+    procedure Print_Prefix (Prefix : Prefix_Type) is
+    begin
+        Put_Line ("Prefix : ");
+        Put_Line (To_Wide_Wide_String (Prefix.Get_Name)
+               & " : <"
+               & To_Wide_Wide_String (Prefix.Get_IRI)
+                    & ">");
+    end Print_Prefix;
+
     procedure Print_Triple (Triple : Triple_Type) is
     begin
-        Put_Line ("Triple: ");
+        Put_Line ("Triple : ");
         Put_Line ("<"
                     & To_Wide_Wide_String (Triple.Get_Subject)
                     & ">");
-        Put_Line ("<" 
+        Put_Line ("<"
                     & To_Wide_Wide_String (Triple.Get_Predicate)
                     & ">");
         Put_Line ("<"
                     & To_Wide_Wide_String (Triple.Get_Object)
                     & ">");
     end Print_Triple;
-    
-    procedure Print_Prefix (Prefix : Prefix_Type) is
-    begin
-        Put_Line ("Prefix: ");
-        Put_Line (To_Wide_Wide_String (Prefix.Get_Name)
-               & ": <"
-               & To_Wide_Wide_Stirng (Prefix.Get_IRI)
-                    & ">");
-    end Print_Prefix;
-    
-    procedure Print_Base (Base : Universal_String) is
-    begin
-        Put_Line ("Base IRI:");
-        Put_Line (To_Wide_Wide_String (Base));
-    end Print_Base;
-    
+
     function Read_File (Path : String) return Wide_Wide_String is
         Buffer : Unbounded_Wide_Wide_String;
         File : File_Type;
@@ -97,7 +99,6 @@ procedure Lexical_Analizer is
     end Read_File;
 
     Lexer : Lexer_Type;
-    Token : Token_Type;
     Source : Source_Type;
 begin
     if Argument_Count < 1 then
@@ -109,10 +110,12 @@ begin
     Source.Initialize (Read_File (Argument (1)));
 
     Lexer.Create (Source);
-    
-    if Turtle_Doc (Lexer) then
-        Put_Line ("Syntactical analysis: This is a valid RDF/Turtle file.");
+
+    if Analyser.Turtle_Doc (Lexer) then
+        Put_Line
+          ("Syntactical analysis : This is a valid RDF/Turtle file.");
     else
-        Put_Line ("Syntactical analysis: This is not a valid RDF/urtle file.");
+        Put_Line
+          ("Syntactical analysis : This is not a valid RDF/urtle file.");
     end if;
-end Lexical_Analizer;
+end Syntactical_Analyser;
