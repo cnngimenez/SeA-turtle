9a321c1214c5f4229a20f14a338272ef70dd1a29
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-08-25 11:37:52 -0300

    Writing the Parser State defined in RDF 1.1 Turtle standard.
    See https://www.w3.org/TR/turtle/#sec-parsing-state for more information.

---
+++ src/turtle/turtle-parser_states.adb
@@ -0,0 +1,83 @@
+--  turtle-parser_states.adb ---
+
+--  Copyright 2020 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+package body Turtle.Parser_States is
+    function Get_Base_URI (Parser_State : Parser_State_Type)
+                          return Universal_String is
+    begin
+        return Parser_State.Base_URI;
+    end Get_Base_URI;
+
+    function Get_Namespaces (Parser_State : Parser_State_Type)
+                      return Namespaces_Type is
+    begin
+        return Parser_State.Namespaces;
+    end Get_Namespaces;
+
+    function Get_BnodeLabels (Parser_State : Parser_State_Type)
+                      return Blank_Node_Labels_Type is
+    begin
+        return Parser_State.BnodeLabels;
+    end Get_BnodeLabels;
+
+    function Get_Cur_Subject (Parser_State : Parser_State_Type)
+                      return Universal_String is
+    begin
+        return Parser_State.Cur_Subject;
+    end Get_Cur_Subject;
+
+    function Get_Cur_Predicate (Parser_State : Parser_State_Type)
+                      return Universal_String is
+    begin
+        return Parser_State.Cur_Predicate;
+    end Get_Cur_Predicate;
+
+    procedure Set_Base_URI (Parser_State : in out Parser_State_Type;
+                            Base_URI : Universal_String) is
+    begin
+        Parser_State.Base_URI := Base_URI;
+    end Set_Base_URI;
+
+    procedure Set_Namespaces (Parser_State : in out Parser_State_Type;
+                              Namespaces : Namespaces_Type) is
+    begin
+        Parser_State.Namespaces := Namespaces;
+    end Set_Namespaces;
+
+    procedure Set_BnodeLabels (Parser_State : in out Parser_State_Type;
+                               BnodeLabels : Blank_Node_Labels_Type) is
+    begin
+        Parser_State.BnodeLabels := BnodeLabels;
+    end Set_BnodeLabels;
+
+    procedure Set_Cur_Subject (Parser_State : in out Parser_State_Type;
+                               Cur_Subject : Universal_String) is
+    begin
+        Parser_State.Cur_Subject := Cur_Subject;
+    end Set_Cur_Subject;
+
+    procedure Set_Cur_Predicate (Parser_State : in out Parser_State_Type;
+                                 Cur_Predicate : Universal_String) is
+    begin
+        Parser_State.Cur_Predicate := Cur_Predicate;
+    end Set_Cur_Predicate;
+
+end Turtle.Parser_States;
---
+++ src/turtle/turtle-parser_states.ads
@@ -0,0 +1,65 @@
+--  turtle-parser_states.ads ---
+
+--  Copyright 2020 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+with Turtle.Namespaces;
+use Turtle.Namespaces;
+
+--  The parser state is defined at the RDF 1.1 Turtle W3C standard (see [1]).
+--  It defines the needed items for parsing triples from turtle statements.
+--
+--  [1] https://www.w3.org/TR/turtle/#sec-parsing-state
+package Turtle.Parser_States is
+
+    type Parser_State_Type is tagged private;
+
+    function Get_Base_URI (Parser_State : Parser_State_Type)
+                          return Universal_State;
+    function Get_Namespaces (Parser_State : Parser_State_Type)
+                            return Namespaces_Type;
+    function Get_BnodeLabels (Parser_State : Parser_State_Type)
+                             return Blank_Node_Labels_Type;
+    function Get_Cur_Subject (Parser_State : Parser_State_Type)
+                             return Universal_State;
+    function Get_Cur_Predicate (Parser_State : Parser_State_Type)
+                               return Universal_State;
+
+    procedure Set_Base_URI (Parser_State : in out Parser_State_Type;
+                            Base_URI : Universal_String);
+    procedure Set_Namespaces (Parser_State : in out Parser_State_Type;
+                              Namespaces : Namespaces_Type);
+    procedure Set_BnodeLabels (Parser_State : in out Parser_State_Type;
+                               BnodeLabels : Blank_Node_Labels_Type);
+    procedure Set_Cur_Subject (Parser_State : in out Parser_State_Type;
+                               Cur_Subject : Universal_String);
+    procedure Set_Cur_Predicate (Parser_State : in out Parser_State_Type;
+                                 Cur_Predicate : Universal_String);
+
+private
+
+    type Parser_State_Type is tagged record
+        Base_URI : Universal_String;
+        Namespaces : Namespaces_Type;
+        BnodeLabels : Blank_Node_Labels_Type;
+        Cur_Subject : Universal_String; --  What type should this be?
+        Cur_Predicate : Universal_String; --  What type should this be?
+    end record;
+
+end Turtle.Parser_States;
